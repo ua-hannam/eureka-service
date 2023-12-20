2b58a21e4b66bbe9ef94bc88a2c58356271bd971
dev-chpark <xpmxf5@gmail.com>
2023-12-20 16:14:14 +0900

    🎉 init: Initial Commit

---
+++ src/test/java/com/example/uahaneurekademo/UaHanEurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uahaneurekademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaHanEurekaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
