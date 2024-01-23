f9cd2b5d99aa58ff73a49003c146f290c326b269
jade <sm.kim2@okestro.com>
2024-01-23 21:03:36 +0900

    :bug:  빌드 에러 픽스

--- src/test/java/com/example/uahaneurekademo/UaHanEurekaApplicationTests.java
+++ src/test/java/com/example/uahaneurekademo/UaHanEurekaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.uahaneurekademo;
 
+import com.example.uahaneureka.UaHanEurekaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UaHanEurekaApplication.class)
 class UaHanEurekaApplicationTests {
 
     @Test
